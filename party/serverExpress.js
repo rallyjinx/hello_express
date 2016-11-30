bcfbc2616a44eeac46c9a8d021649b23c9a59fda
Rally Jinx <rsjinx@gmail.com>
2016-11-30 11:58:00 -0600

    remove node_modules

--- party/serverExpress.js
+++ party/serverExpress.js
@@ -12,15 +12,16 @@ app.disable('x-powered-by');
 
 app.get('/guests', function(req, res) {
   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
-      if (err) {
-        console.error(err.stack);
-        return res.sendStatus(500);
-      }
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
 
-  var guests = JSON.parse(guestsJSON);
-  res.send(guests);
-  )};
-)};
+    var guests = JSON.parse(guestsJSON);
+
+    res.send(guests);
+  });
+});
 
 app.use(function(req, res) {
   res.sendStatus(404);
